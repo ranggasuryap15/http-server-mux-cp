f6a79cad1339b22e2189bebf8e3c56b9a4c87ab1
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-05-01 14:52:50 +0700

    finish exercise

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func TimeHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		now := time.Now()
+		weekday := now.Weekday()
+		day := now.Day()
+		month := now.Month()
+		year := now.Year()
+		result := fmt.Sprintf("%s, %d %s %d", weekday, day, month, year)
+
+		w.Write([]byte(result))
+	}
 }
 
 func SayHelloHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			w.Write([]byte("Hello there"))
+		} else {
+			w.Write([]byte("Hello, " + name + "!"))
+		}
+	}
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	// TODO: answer here
+	mux.HandleFunc("/time", TimeHandler())
+	mux.HandleFunc("/hello", SayHelloHandler())
+	
 	return mux
 }
 
